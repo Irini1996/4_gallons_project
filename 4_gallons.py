b0a4c3549461f7676735f5e826b504b25e9c5fe9
Irini1996 <iringega@gmail.com>
2025-09-28 01:31:14 +0200

    4_gallons.py

--- 4_gallons.py
+++ 4_gallons.py
@@ -5,7 +5,7 @@
 
 
 def solve(a=0, b=0, seen=set(), path=[]):
-    if a + b == 4:  # Goal: stop when the two buckets together hold exactly 4 gallons
+    if a + b == 4 or a==4: # Goal: stop when the two buckets together hold exactly 4 gallons
         return path
     seen.add((a, b)) # Remember the current state (to avoid visiting it again)           
 
@@ -27,4 +27,5 @@ def solve(a=0, b=0, seen=set(), path=[]):
 #run and print the steps
 for i, (act, state) in enumerate(solve(), 1):
     print(f"{i}) {act} -> {state}")
+
 
